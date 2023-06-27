a02b0b7004aa960b6fab6eb8b2be4aa2f06a699f
samuelsachin1 <119583736+samuelsachin1@users.noreply.github.com>
2023-06-27 20:34:07 +0530

    Add files via upload
    Frontend pages folder

---
+++ AllCoursesUser.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavbarUser from '../components/NavbarUser'
+import Quesalluser from '../components/Quesalluser'
+
+function AllCoursesUser() {
+  return (
+    <div>
+        <NavbarUser/>
+        <Quesalluser/>
+    </div>
+  )
+}
+
+export default AllCoursesUser
